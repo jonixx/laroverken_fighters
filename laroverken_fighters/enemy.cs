dfb2ba297951ea7010dc7723b709bbda64f46cfb
jonixx <jonixx@users.noreply.github.com>
2018-01-11 15:23:07 +0100

    added enemy class

---
+++ laroverken_fighters/enemy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laroverken_fighters
+{
+    //En class funkar som en beskrivning av någonting
+    //Här beskriver vi hur en fiende fungerar och
+    //vilka egenskaper den har
+    class Enemy
+    {
+        public int hp;
+        public int dmg;
+        public string name;
+              
+        //Initialize, sätt värden på variablerna
+        public void Setup()
+        {
+            Random randomness = new Random();
+
+            hp = randomness.Next(6, 15);
+            dmg = randomness.Next(2, 6);
+
+            string[] namesToPick =
+            {
+                "String Danne", //0
+                "Last King", //1
+                "Gunnar", //2
+                "CSN", //3
+                "Skolverket" //4
+                
+            };
+
+            name = namesToPick[randomness.Next(0, namesToPick.Length)];
+
+        }
+    }
+}
